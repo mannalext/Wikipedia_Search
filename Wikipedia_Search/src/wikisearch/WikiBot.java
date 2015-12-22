382c1becef00115129c40855537d0533f91de792
southheat92 <southheat92@gmail.com>
2015-12-22 16:35:34 -0500

    apache JAR setup
    adding the JARs to the build path and importing them to WikiBot.java

--- Wikipedia_Search/src/wikisearch/WikiBot.java
+++ Wikipedia_Search/src/wikisearch/WikiBot.java
@@ -1,4 +1,15 @@
 package wikisearch;
+import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.TrustStrategy;
 
 public class WikiBot {
 
