7a63921e062a824ad9fffcfdcc7cf510475eb817
southheat92 <southheat92@gmail.com>
2015-12-22 18:29:59 -0500

    first API call, no massaging
    makes a call to the api fetching the first 500 links from the article
    titled “Mars”. this data needs to be parsed for “continues” and link
    storage

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Wikipedia_Search
 a repo for a "bacon number" game using wikipedia articles
+
+
+THIS PROJECT IS UNDER ACTIVE DEVELOPMENT
+
+
+12/22/15
+Made the original commits, adding JARs and basic logic to make calls against the API.
+
+Right now a call to return the first 500 links on the “Mars” article is made. The API return value gets stored in “rawResultAsString”. The plan is to parse this, checking for the existence of a continue, and storing the 500 links. If a continue was found, repeat recursively until one is not found, always storing the 500 additional links.
+
+After that, I will have all the links from the original article. That same logic will need to be called on all links from that article, recursively, until the second article name is found among the links. Then, return the number of links between articles A and B.
+
+
+
+
+
+TODO: 
+1. Parse the results of an API call. 
+	- Create the infrastructure to run as many continues as necessary while 		adding links to the result		
+	- Store links from each call to continue
+
+2. Once you have all the links from article A, test how long it would take to run the same logic (make a call, store links from the page) for every link found on article A. If the time trivial, this project can be done without recursion. Otherwise, develop the recursive / multithreaded algorithm. 
+
--- Wikipedia_Search/src/wikisearch/WikiBot.java
+++ Wikipedia_Search/src/wikisearch/WikiBot.java
@@ -1,7 +1,12 @@
 package wikisearch;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
@@ -10,11 +15,46 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.http.ssl.TrustStrategy;
+import org.apache.http.client.HttpClient;
+
 
 public class WikiBot {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClientProtocolException, IOException {
 
+		String endpoint = "https://en.wikipedia.org/w/api.php?";
+		String action = "action=query";
+		String prop = "prop=links";
+		String format = "format=json";
+		String namespace = "plnamespace=0";
+		String limit = "pllimit=500&";
+		String order = "pldir=ascending";
+		String plcontinue = "plcontinue=14640471%7C0%7CIbragimov_(Martian_crater)"; //the plcontinue argument replaces order in a new query which uses it
+		String rawcontinue = "rawcontinue=";
+		String titleA = "titles=Mars";	
+		String delimiter = "&";
+		
+		StringBuilder urlConstruction = new StringBuilder();
+		urlConstruction.append(endpoint + action + delimiter + prop + delimiter + format + delimiter + namespace + delimiter + limit + delimiter + order + delimiter + rawcontinue + delimiter + titleA);
+		String url = urlConstruction.toString();
+		
+		HttpClient client = HttpClientBuilder.create().build();
+		HttpGet request = new HttpGet(url);
+		HttpResponse httpResponse = client.execute(request);
+		
+		BufferedReader rd = new BufferedReader(
+				new InputStreamReader(httpResponse.getEntity().getContent()));
+
+			StringBuffer rawResultAsBuffer = new StringBuffer();
+			String line = "";
+			while ((line = rd.readLine()) != null) {
+				rawResultAsBuffer.append(line);
+			}
+			
+		String rawResultAsString = rawResultAsBuffer.toString();
+
+		System.out.println("debug");
+		
 	}
 
 }
